5044f1aca183cf0b1159ad5b1f9bea745e5bd2e8
It22913210 <it22913210@my.sliit.lk>
2025-03-16 20:54:48 +0530

    created milkingdata.js model

---
+++ EverFreshDairyFarm/Backend/models/milkingData.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const milkingDataSchema = new mongoose.Schema({
+
+    milkBatchId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    amountofMilk: {
+        type: Number,
+        required: true 
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    qualityCheckResult: {
+        type: String,
+        enum: ['pass', 'Fail'],
+        required: true 
+    },
+
+}
+);
+
+const milkingData = mongoose.model('milking', milkingDataSchema);
+module.exports = milkingData;
