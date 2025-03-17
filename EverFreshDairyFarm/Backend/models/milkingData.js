7107d04f331424d13972ce80c2b5c7ffe7e3d49b
It22913210 <it22913210@my.sliit.lk>
2025-03-17 22:30:02 +0530

    milkingData

--- EverFreshDairyFarm/Backend/models/milkingData.js
+++ EverFreshDairyFarm/Backend/models/milkingData.js
@@ -2,24 +2,13 @@ import mongoose from "mongoose";
 
 const milkingDataSchema = new mongoose.Schema({
 
-    milkBatchId: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    amountofMilk: {
-        type: Number,
-        required: true 
-    },
-    duration: {
-        type: Number,
-        required: true
-    },
-    qualityCheckResult: {
-        type: String,
-        enum: ['pass', 'Fail'],
-        required: true 
-    },
+    milkBatchId: {type: Number, required: true, unique: true},
+    amountofMilk: {type: Number, required: true },
+    duration: {type: Number, required: true},
+    date: {type: Date, required: true},
+    milkYield: {type: Number, required: true},
+    qualityCheckResult: { type: String, enum: ['pass', 'Fail'], required: true },   
+    notes: {type: String , required: true},
 
 }
 );
