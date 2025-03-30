925bf5d38379e5cc1f0ef98c01270cab31fd8af6
Sithija-Ranathunga <131248698+Sithija-Ranathunga@users.noreply.github.com>
2025-03-30 13:40:09 +0530

    All the new working updates

--- EverFreshDairyFarm/Frontend/src/components/Header.jsx
+++ EverFreshDairyFarm/Frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ export function Header() {
   const { userData } = useContext(AppContent);
 
   return (
-    <nav className="bg-neutral-400 p-4 shadow-md relative z-50">
+    <nav className="bg-white p-4 shadow-md relative z-50">
 
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
